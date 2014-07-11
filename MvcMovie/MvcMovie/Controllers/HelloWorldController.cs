625703890e9fb02181fc581379f9c083a61cf126
Said Ouarrich <said.ouarrich@sage.com>
2014-07-11 18:02:19 +0200

    Adding Controller

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my <b>Default</b> action...";
+        }
+
+        public string Welcome(string name, int age)
+        {
+            return "Welocome to "+name+ " page. He is "+age+ " years old";
+        }
+    }
+}
